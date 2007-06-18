34d23e268078e1b1ca6aba9a0525a4a139765657
tobias <tobias@12be777f-adf9-0310-842f-e37ecc4c7426>
2007-06-18 18:22:38 +0000

    - testing-tree / vdr-1.4: now resetting the cam once if initialization fails, should avoid some 0-byte-recordings on pay-tv-channels

--- menu.c
+++ menu.c
@@ -4615,8 +4615,9 @@ void cMenuSetupLNB::LoadActuall()
 
 // --- cMenuSetupCICAM -------------------------------------------------------
 
-bool camFound[3]; //TB: has the CAM been found successfully?!
-time_t firstTimeChecked[3]; //TB: first time we did check - used for deminining when we assume an error
+static bool camFound[3]; //TB: has the CAM been found successfully?!
+static bool camResetted[3] = {false, false, false}; //TB: has the CAM already been resetted?
+static time_t firstTimeChecked[3]; //TB: first time we did check - used when we assume an error
 
 class cMenuSetupCICAMItem : public cOsdItem {
 private:
@@ -4642,6 +4643,13 @@ cMenuSetupCICAMItem::cMenuSetupCICAMItem(int Device, cCiHandler *CiHandler, int 
 	  firstTimeChecked[slot] = time(NULL);
       } else {
 	CamName = tr("Error");
+        Skins.Message(mtWarning, tr("Resetting CAM..."));
+        if (!camResetted[slot] && CiHandler->Reset(slot)) {
+          Skins.Message(mtInfo, tr("CAM has been reset"));
+          camFound[slot] = 0;
+	  camResetted[slot] = true;
+        } else
+          Skins.Message(mtError, tr("Can't reset CAM!"));
       }
     } else {
       CamName = "-";
