1babd63506b7f4b2aa0b2114ad501743a50ce285
tobias <tobias@12be777f-adf9-0310-842f-e37ecc4c7426>
2007-08-10 07:46:14 +0000

    - testing-tree / vdr-1.4: reapplied my reverted speedup-patch

--- tools.c
+++ tools.c
@@ -158,21 +158,15 @@ char *strreplace(char *s, const char *s1, const char *s2)
      int l1 = strlen(s1);
      int l2 = strlen(s2);
      if (l2 > l1)
-        s = (char *)realloc(s, strlen(s) + l2 - l1 + 1);
+        s = (char *)realloc(s, l + l2 - l1 + 1);
+     char *sof = s + of;
      if (l2 != l1)
-        memmove(s + of + l2, s + of + l1, l - of - l1 + 1);
-     strncpy(s + of, s2, l2);
+        memmove(sof + l2, sof + l1, l - of - l1 + 1);
+     strncpy(sof, s2, l2);
      }
   return s;
 }
-#if 0
-char *skipspace(const char *s)
-{
-  while (*s && isspace(*s))
-        s++;
-  return (char *)s;
-}
-#endif
+
 char *stripspace(char *s)
 {
   if (s && *s) {
@@ -252,21 +246,22 @@ bool isempty(const char *s)
 
 int numdigits(int n)
 {
-  char buf[16];
-  snprintf(buf, sizeof(buf), "%d", n);
-  return strlen(buf);
+  int res = 1;
+  while (n >= 10) {
+        n /= 10;
+        res++;
+        }
+  return res;
 }
 
 bool isnumber(const char *s)
 {
   if (!*s)
      return false;
-  while (*s) {
-        if (!isdigit(*s))
-           return false;
-        s++;
-        }
-  return true;
+  do {
+     if (!isdigit(*s))
+        return false;
+     } while (*++s);
 }
 
 cString AddDirectory(const char *DirName, const char *FileName)
